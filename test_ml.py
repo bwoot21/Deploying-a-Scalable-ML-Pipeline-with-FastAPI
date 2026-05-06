c844925e3757c39a1cc46251be257dd58ba2bbc4
Blaine Wooten <bwoot21@wgu.edu>
2026-05-06 02:24:04 +0000

    Implemented first test

--- test_ml.py
+++ test_ml.py
@@ -1,14 +1,15 @@
 import pytest
+from sklearn.ensemble import RandomForestClassification
+from sklearn.datasets import make_classification
 # TODO: add necessary import
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
-    """
-    # add description for the first test
-    """
-    # Your code here
-    pass
-
+# Test the training model function.
+# Used this website as a reference to learn how to do it: https://medium.com/@ydmarinb/simplifying-unit-testing-in-machine-learning-with-python-df9b9c1a3300
+# Also used https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html#sklearn.ensemble.RandomForestClassifier.predict for proper exporting syntax.
+def test_training_model():
+    X_train, y_train = make_classification(n_samples = 300, n_features = 3, random_state = 42)
+    return_model = train_model(X_train,y_train)
+    assert hasattr(return_model, "feature_importances_"), "Model needs to have feature_importances_ attribute"
 
 # TODO: implement the second test. Change the function name and input as needed
 def test_two():
@@ -16,6 +17,7 @@ def test_two():
     # add description for the second test
     """
     # Your code here
+    
     pass
 
 
