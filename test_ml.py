02b24487712fc47a19a3aacdfc0108afb6830f7a
Blaine Wooten <bwoot21@wgu.edu>
2026-05-06 02:36:07 +0000

    Implemented first test

--- test_ml.py
+++ test_ml.py
@@ -8,7 +8,7 @@ from ml.model import train_model
 # Used this website as a reference to learn how to do it: https://medium.com/@ydmarinb/simplifying-unit-testing-in-machine-learning-with-python-df9b9c1a3300
 # Also used https://scikit-learn.org/stable/modules/generated/sklearn.ensemble.RandomForestClassifier.html#sklearn.ensemble.RandomForestClassifier.predict for proper exporting syntax.
 def test_training_model():
-    X_train, y_train = make_classification(n_samples = 300, n_features = 3, random_state = 42)
+    X_train, y_train = make_classification(n_samples = 300, n_features = 5, random_state = 42)
     return_model = train_model(X_train,y_train)
     assert hasattr(return_model, "feature_importances_"), "Model needs to have feature_importances_ attribute"
 
